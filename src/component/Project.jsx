b133a3bb5bcee2e12bfa67e6ecf7f71a92c98381
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-15 22:01:28 +0700

    first doing in Project/ProjectItem.jsx

---
+++ src/component/Project.jsx
@@ -0,0 +1,14 @@
+
+
+const Project = () => {
+  return (
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+        <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+        <p className="text-center py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ducimus sunt labore ex? Dicta ad eius labore vitae nulla ullam atque fugiat? Officiis, rem sit. 
+        Accusamus laudantium sed voluptatibus quisquam mollitia!</p>
+    </div>
+  )
+}
+
+export default Project
