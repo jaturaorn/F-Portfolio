7434d2e59950f9e931a95d2731dceb73a2fa7f1f
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-05-17 15:03:25 +0700

    do on project-item.jsx

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,12 +1,17 @@
+import ProjectItem from "./ProjectItem"
 
 
 const Project = () => {
   return (
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
         <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
-        <p className="text-center py-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Ducimus sunt labore ex? Dicta ad eius labore vitae nulla ullam atque fugiat? Officiis, rem sit. 
-        Accusamus laudantium sed voluptatibus quisquam mollitia!</p>
+        <p className="text-center py-8">I project that i have make</p>
+        <div className="grid sm:grid-cols-2 gap-6">
+          <ProjectItem img='' title='' />
+          <ProjectItem img='' title='' />
+          <ProjectItem img='' title='' />
+          <ProjectItem img='' title='' />
+        </div>
     </div>
   )
 }
