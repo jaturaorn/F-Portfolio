deb3441506b5d2ff4fb38e563296684326af3dc7
Jaturaporn Sutongkeaw <Jaturapornff@gmail.com>
2023-07-20 20:00:54 +0700

    add new link to  web-project

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,19 +1,52 @@
-import ProjectItem from "./ProjectItem"
-
+import ProjectItem from "./ProjectItem";
+import bem from "../Images/Capture.jpg";
+import tin from "../Images/Capture1.png";
+import coma from "../Images/Capture2.jpg";
+import game from "../Images/Capture1.jpg";
+import count from "../Images/Capture3.jpg";
+import drum from "../Images/Capture4.jpg";
 
 const Project = () => {
   return (
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-        <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
-        <p className="text-center py-8">I project that i have make</p>
-        <div className="grid sm:grid-cols-2 gap-6">
-          <ProjectItem img='' title='' />
-          <ProjectItem img='' title='' />
-          <ProjectItem img='' title='' />
-          <ProjectItem img='' title='' />
-        </div>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
+      <p className="text-center py-8">I project that i have make</p>
+      <div className="grid sm:grid-cols-2 gap-6">
+        <ProjectItem
+          img={bem}
+          title="BEM Clone Project"
+          link="https://bem-clone-project.netlify.app"
+        />
+        <ProjectItem
+          img={tin}
+          title="Tindog-Project"
+          link="https://fabulous-biscochitos-86b7e2.netlify.app"
+        />
+        <ProjectItem
+          img={coma}
+          title="Colma-academy-project"
+          link="https://coma-academy.netlify.app/"
+        />
+        <ProjectItem
+          img={game}
+          title="Game-findyourhat-Js"
+          link="https://replit.com/@JaturapornGoo/Jsdasset"
+        />
+        <ProjectItem
+          img={count}
+          title="Count-project"
+          link="https://beamish-blancmange-f95b08.netlify.app/"
+        />
+        <ProjectItem
+          img={drum}
+          title="Drum-kit-Project"
+          link="https://drum-f-0c4a41.netlify.app/"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,36 +1,41 @@
 import WorkItem from "./WorkItem";
 
-
 const Work = () => {
   const data = [
     {
-        year: '2021-Persent',
-        title: 'Broadcast Production Unit , Director Assistant',
-        detail:
-        'at New Nimmitmai Church, served as a live broadcast system controller and camera operator, resulting in a 50% increase in data from users via social media Controlled image and camera systems at the church, resulting in a 40% increase in data from users via social media and in-person visitors'
+      year: "2021-Present",
+      title: "Broadcast Production Unit , Director Assistant",
+      detail:
+        "at New Nimmitmai Church, served as a live broadcast system controller and camera operator, resulting in a 50% increase in data from users via social media Controlled image and camera systems at the church, resulting in a 40% increase in data from users via social media and in-person visitors",
     },
     {
-      year: 'March 2023',
-      title: 'Amity Hackathon March 2023',
+      year: "March 2023",
+      title: "Amity Hackathon March 2023",
       detail:
-      'Participated in the Amity Hackathon, a generative AI hackathon with a focus on social impact. Developed an app idea that enables blind people to create art using GPT and midjourneyAI technology, promoting creativity and social engagement'
+        "Participated in the Amity Hackathon, a generative AI hackathon with a focus on social impact. Developed an app idea that enables blind people to create art using GPT and midjourneyAI technology, promoting creativity and social engagement",
     },
     {
-      year: 'February-May 2023',
-      title: 'Junior Software Developer Program cohort 4',
+      year: "February-May 2023",
+      title: "Junior Software Developer Program cohort 4",
       detail:
-      'Training in online Bootcamp Junior Software Developer Program cohort 4 at Generation Thailand'
-    }
-];
+        "Training in online Bootcamp Junior Software Developer Program cohort 4 at Generation Thailand",
+    },
+  ];
   return (
     <div id="work" className="max-x-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Experience/Trianing</h1>
-      {data.map((item,idx) => (
-        <WorkItem key={idx} year={item.year} 
-        title={item.title} detail={item.detail} />
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Experience/Trianing
+      </h1>
+      {data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          detail={item.detail}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
